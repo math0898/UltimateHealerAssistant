4227ea89b9755f05257e86506c9fa74a531c1136
math0898 <25396616+math0898@users.noreply.github.com>
2025-05-15 11:52:37 -0500

    Sanity Check

--- src/main/java/io/github/math0898/Main.java
+++ src/main/java/io/github/math0898/Main.java
@@ -1,7 +1,29 @@
 package io.github.math0898;
 
+import java.io.InputStream;
+import java.text.NumberFormat;
+import java.util.Scanner;
+
 public class Main {
+
+    private final static String TEST_FILE = "/WoWCombatLog-051425_164349.txt";
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try (InputStream inputStream = Main.class.getResourceAsStream(TEST_FILE)) {
+            if (inputStream == null) return;
+            Scanner s = new Scanner(inputStream);
+            long startTime = System.currentTimeMillis();
+            int i = 0;
+            while (s.hasNextLine()) {
+                String line = s.nextLine();
+                i++;
+                if (line.contains("ENCOUNTER_END"))
+                    System.out.println(line);
+            }
+            long endTime = System.currentTimeMillis();
+            System.out.println("Read " + NumberFormat.getInstance().format(i) + " lines. Took: " + NumberFormat.getInstance().format((endTime - startTime)) + "ms");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
